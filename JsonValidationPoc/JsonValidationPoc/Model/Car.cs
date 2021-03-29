bf56ca4d0a1eb4ad52e8a5359cb138fa2d70d2a2
M4tt22 <matthieu.pomies40@gmail.com>
2021-03-29 22:43:28 +0200

    making json deserialization

---
+++ JsonValidationPoc/JsonValidationPoc/Model/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace JsonValidationPoc.Model
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int EnginePower { get; set; }
+    }
+}
--- JsonValidationPoc/JsonValidationPoc/Program.cs
+++ JsonValidationPoc/JsonValidationPoc/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JsonValidationPoc.Model;
+using System;
+using System.IO;
+using System.Text.Json;
 
 namespace JsonValidationPoc
 {
@@ -10,6 +9,17 @@ namespace JsonValidationPoc
     {
         static void Main(string[] args)
         {
+
+            string jsonPath = "Json/car.json";
+
+            if (File.Exists(jsonPath))
+            {
+                Car car = JsonSerializer.Deserialize<Car>(jsonPath);
+                if (car != null)
+                {
+                    Console.WriteLine($"Car : Brand : {car.Brand} ; {car.Model} ; {car.EnginePower}");
+                }
+            }
         }
     }
 }
